27d774391c53e475c64fafd330d6cc243d147634
consher <c_sheridan@mailbox.org>
2023-08-18 13:04:25 +0100

    added error box feature to anti_coincidence test

--- anti_coincidence.py
+++ anti_coincidence.py
@@ -3,14 +3,16 @@ import numpy as np
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-f1',   dest='file1',   type=str,   help='1st file in .npy format')
-parser.add_argument('-f2',   dest='file2',   type=str,   help='2nd file in .npy format to compare with 1st')
-parser.add_argument('-o',   dest='output',   type=str,   help='output file name')
+parser.add_argument('-f1',  dest = 'file1',   type = str,   help = '1st file in .npy format')
+parser.add_argument('-f2',  dest = 'file2',   type = str,   help = '2nd file in .npy format to compare with 1st')
+parser.add_argument('-o',   dest = 'output',  type = str,   help = 'output file name')
+parser.add_argument('-err', dest = 'error',   type = int,   help = 'error value for anti-coincidence (default = 0)', default = 0)
 
 args = parser.parse_args()
 f1 = args.file1
 f2 = args.file2
 output = args.output
+err = args.error
 
 print('Comparing {0} and {1} potential hits.'.format(f1, f2))
 
@@ -24,11 +26,42 @@ tally = 0
 
 for i in range(0, hits1.shape[0]):      #iterates through each value of hit1 and hit2
     for j in range(0, hits1.shape[1]):
-        if(hits1[i][j] == hits2[i][j] and hits1[i][j] == True):  
-            #if both equal one then pass anti-coincidence test
-            tally += 1
+        #begins test if hits1[i][j] is True
+        if(hits1[i][j] == True):
+
+                if(err > 0):        
+                    #if the error margin is greater than 1
+                    #it checks the box around hits1[i][j]
+
+                    check = False
+
+                    for p in range(-err, err + 1):
+                        for q in range(-err, err + 1):
+                            
+                            if(hits1[i][j] == hits2[i + p][j + q]):
+                                check = True    
+                                #if the box around hits1[i][j] contains a match check is True
+
+                    #passes anti-coincidence test
+                    if(check == True):
+                        tally += 1
+
+                    #otheriwse sets both to false
+                    else:
+                        hits1[i][j] = False
+                        hits2[i][j] = False        
+
+                #if the error value is 1, just compares the exact corresponding hits2 value
+                elif(err == 0):
+                    if(hits1[i][j] == hits2[i][j]):
+                        tally += 1
+
+                    else:
+                        hits1[i][j] = False
+                        hits2[i][j] = False
+
+        #if hits1 is False, simply sets both to zero
         else:
-            #otherwise both set to false
             hits1[i][j] = False
             hits2[i][j] = False
 
